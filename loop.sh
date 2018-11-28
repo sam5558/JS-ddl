22e035bfa6eeb070501fc5c732fb3141b584f9d3
sam5558 <sam5558@users.noreply.github.com>
2018-11-28 13:44:24 +0000

    Update loop.sh

--- loop.sh
+++ loop.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
 root=$1
-echo $root
 first=$2
-echo $first
 last=$3
-echo $last
-#length = 2
 
-for i in $(seq $first $last); do python ~/Téléchargements/tz/createepun.py "$root/$i"; done
-#for i in $(seq first last); do python ~/Téléchargements/tz/createepun.py "https://www.japscan.cc/lecture-en-ligne/kingdom/$i"; done
+for i in $(seq $first $last); do python createcbz.py "$root/$i"; done
