b04c0c04585fd3136bd71cb1e5815e5e1c3f2e84
sam5558 <sam5558@users.noreply.github.com>
2018-12-01 22:54:26 +0000

    Update loop.sh

--- loop.sh
+++ loop.sh
@@ -6,4 +6,10 @@ root=$1
 first=$2
 last=$3
 
+if [[ $1 = "-v"]]; then
+
+for i in $(seq $first $last); do python createcbz.py "$root/volume-$i"; done
+
+else
+
 for i in $(seq $first $last); do python createcbz.py "$root/$i"; done
